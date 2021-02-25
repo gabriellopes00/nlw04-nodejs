12a88bc799c610c943350596d164a11508e3267c
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-25 15:22:26 -0300

    create surveys-users controller

---
+++ src/controllers/surveys-users-controller.ts
@@ -0,0 +1,31 @@
+import { SurveysRepository } from '../database/repositories/surveys'
+import { UsersRepository } from '../database/repositories/users'
+import { getCustomRepository } from 'typeorm'
+import { Request, Response } from 'express'
+import { SurveysUsersRepository } from '../database/repositories/surveys-users'
+
+export class SendMailController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { email, surveyId } = req.body
+
+    const usersRepository = getCustomRepository(UsersRepository)
+    const surveysRepository = getCustomRepository(SurveysRepository)
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
+
+    const existentUser = await usersRepository.findOne({ email })
+    if (!existentUser)
+      return res.status(401).json({ message: 'User not found' })
+
+    const existentSurvey = await surveysRepository.findOne({ id: surveyId })
+    if (!existentSurvey)
+      return res.status(400).json({ message: 'Survey not exists' })
+
+    const surveysUser = surveysUsersRepository.create({
+      user_id: existentUser.id,
+      survey_id: surveyId
+    })
+    await surveysUsersRepository.save(surveysUser)
+
+    return res.json({ surveysUser })
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express'
+import { SendMailController } from './controllers/surveys-users-controller'
 import { SurveysController } from './controllers/surveys-controller'
 import { UsersController } from './controllers/users-controller'
 
 const usersController = new UsersController()
 const surveysController = new SurveysController()
+const sendMailController = new SendMailController()
 
 const router = Router()
 
 router.post('/users', usersController.create)
+
+router.post('/surveys-users', sendMailController.handle)
 
 router.post('/surveys', surveysController.create)
 router.get('/surveys', surveysController.listAll)
