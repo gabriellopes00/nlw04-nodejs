e4b5504636827cb027487a090c0423be525f8613
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-26 14:35:37 -0300

    create users data validation

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm'
+import * as yup from 'yup'
 import { UsersRepository } from '../database/repositories/users'
 
 export class UsersController {
   async create(req: Request, res: Response): Promise<Response> {
     const { name, email } = req.body
     const usersRepository = getCustomRepository(UsersRepository)
+
+    const isValidData = await yup
+      .object()
+      .shape({
+        name: yup.string().required(),
+        email: yup.string().required().email()
+      })
+      .isValid(req.body)
+
+    if (!isValidData)
+      return res.status(400).json({ error: 'Validation failed' })
 
     const userAlreadyExists = await usersRepository.findOne({ email })
     if (userAlreadyExists) {
